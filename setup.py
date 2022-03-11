2408671a22401b93a45ce45f6059fc2cb60a2e1e
arloreinforcement <100693979+arloreinforcement@users.noreply.github.com>
2022-03-11 09:24:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='ARLO',
       url='https://arlo-lib.github.io/arlo-lib/',
       install_requires=['catboost==1.0.3', 'cloudpickle==1.6.0', 'gym==0.19.0', 'joblib==1.1.0', 'matplotlib==3.5.0',
                         'mushroom_rl==1.7.0', 'numpy==1.22.0', 'optuna==2.10.0', 'plotly==5.4.0', 'scikit_learn==1.0.2', 
-                        'scipy==1.7.3', 'torch==1.10.1', 'xgboost==1.5.1'],
+                        'scipy==1.7.3', 'torch==1.10.1', 'xgboost==1.5.1', 'mujoco-py==2.1.2.14'],
       classifiers=['Programming Language :: Python :: 3',
                    'License :: MIT License',
                    'Operating System :: OS Independent'
